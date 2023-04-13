Added decorrelation objective + fixed identity matrix to not penalize silent units

models.py
@@ -93,11 +93,21 @@ class Conv_AE(nn.Module):
 
         recon_loss = criterion(y_pred, y_true)
         
+        # To enforce orthonormality (i.e., units form an orthonormal set of basis vectors), a.k.a. high-dimensional PCA, SVD, or Sanger's rule (GHA).
         batch_size, hidden_dim = hidden.shape
-        gram = torch.mm(hidden.t(), hidden) / (batch_size*hidden_dim)  # Compute the Gram matrix of the hidden layer's activations
-        diff = gram - torch.eye(hidden_dim, device='cuda')   # Compute the Frobenius norm of the difference between the Gram matrix and the identity matrix
-        orth_loss = orth_alpha * torch.norm(diff, p='fro')
-        
+        gram = torch.mm(hidden.t(), hidden)  # Compute the Gram matrix of the hidden layer's activations
+        #identity = torch.eye(hidden_dim, device='cuda')
+        identity = torch.diag( (hidden.sum(dim=0) != 0).float() )
+        orth_loss = orth_alpha * torch.norm(gram - identity, p='fro') / (batch_size*hidden_dim) # Compute the Frobenius norm of the difference between the Gram matrix and the identity matrix
+
+        '''
+        # To enforce pairwise decorrelation (i.e., orthogonality, not orthonormality).
+        batch_size, hidden_dim = hidden.shape
+        gram = torch.mm(hidden.t(), hidden)
+        mask = 1 - torch.eye(hidden_dim, device='cuda')
+        orth_loss = orth_alpha * torch.sum((gram*mask)**2) / (batch_size*hidden_dim)
+        '''
+
         l1_penalty = L1_lambda * hidden.abs().sum()
 
         if self.hard_sparsity: