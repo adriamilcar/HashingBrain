ecb3d41c009ad713954b4063022d9bc4f9bf3b9c
adriamilcar <adrianfernandezamil@gmail.com>
2024-04-16 01:55:09 +0200

    added hull polygon analyses and image space overlap metric

--- utils.py
+++ utils.py
@@ -434,6 +434,28 @@ def euclidean_distance(point1, point2):
     return dist
 
 
+def cosine_similarity(vec1, vec2):
+    """
+    Calculate the cosine similarity between two vectors.
+    
+    Args:
+        vec1, vec2 (1D numpy array): Vectors.
+
+    Returns:
+        similarity (float): Cosine similarity between vec1 and vec2.
+    """
+    dot_product = np.dot(vec1, vec2)
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+    
+    if norm_vec1 == 0 or norm_vec2 == 0:
+        return 0
+    
+    similarity = dot_product / (norm_vec1 * norm_vec2)
+    
+    return similarity
+
+
 def hashing_data(dataset, in_bits=True):
     '''
     Performs perceptual hashing over all images in the dataset.
